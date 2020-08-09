5e509eb05166da7759cdbabd13a4a03e26712eb0
julian libor <julian.libor@gmail.com>
2020-08-09 21:50:13 +0200

    init

---
+++ src/components/useAuthors.js
@@ -0,0 +1,28 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useAuthors = () => {
+    const { settingsJson } = useStaticQuery(
+        graphql`
+            query authorsQuery {
+                settingsJson(
+                    fileRelativePath: { eq: "/content/settings/authors.json" }
+                ) {
+                    ...authors
+                }
+            }
+        `,
+    );
+
+    return settingsJson.authors;
+};
+
+export const authorsFragment = graphql`
+    fragment authors on SettingsJson {
+        authors {
+            email
+            name
+            id
+            link
+        }
+    }
+`;
